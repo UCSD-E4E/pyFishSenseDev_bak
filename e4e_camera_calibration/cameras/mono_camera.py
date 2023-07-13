2c4a8978458718b667fdb9dc09916bec41175517
Viva <vivaprojects@gmail.com>
2023-07-13 14:19:06 -0700

    Added monocamera calibration files into the pipeline. Added several comments to calibration files.

---
+++ e4e_camera_calibration/cameras/mono_camera.py
@@ -0,0 +1,11 @@
+from abc import ABC
+from e4e_camera_calibration.cameras.camera import Camera
+
+
+class MonoCamera(Camera, ABC):
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
+
+    @property
+    def number_of_sensors(self) -> int:
+        return 1
