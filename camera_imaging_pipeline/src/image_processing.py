5feadb6548fdf03afbabc63eefb88e404b742fb8
Viva <vivaprojects@gmail.com>
2023-07-14 11:40:27 -0700

    Displaying laser detection with Hamish's color corrected images:

--- camera_imaging_pipeline/src/image_processing.py
+++ camera_imaging_pipeline/src/image_processing.py
@@ -1,7 +1,7 @@
 import numpy as np
 import rawpy
 import cv2 as cv
-from utils.h_functions import *
+from camera_imaging_pipeline.utils.h_functions import *
 
 #this class allows the user to configure a image processing pipeline by adjusting
 #the paramters in the params1.json file. Creat a new .json for each desired 
@@ -22,28 +22,54 @@ class imageProcessing():
     #The method returns the image both in it's original size for further processing, 
     #and it returns the resized data for displaying the processed image conveniently to 
     #the user.
-    def applyToImage(self, img_path):
-        self.raw = rawpy.imread(img_path)
-        self.img = self.raw.raw_image.copy()
+    def applyToImage(self, img):
 
-        if self.processes['linearization'] == True:
-            self.img = linearization(self.img)
-        if self.processes['demosaic'] == True:
-            self.img = demosaic(self.img)
-        if self.processes['denoising'] == True:
-            self.img = denoising(self.img, self.denoising_val)
-        if self.processes['colorSpace'] == True:
-            self.img = colorSpace(self.img, self.colour)
-        if self.processes['exposureComp'] == True:
-            self.img = exposureComp(self.img, self.exposure_val)
-        if self.processes['toneCurve'] == True:
-            self.img = toneCurve(self.img, self.tone_curve)
-        if self.processes['gammaCorrection'] == True:
-            self.img = gammaCorrection(self.img, self.gamma_correction)
-        if self.processes['greyWorldWB'] == True:
-            self.img = greyWorldWB(self.img, self.colour)
-        
-        return self.img ,imageResize(self.img, self.resize_val)
+        if type(img) == str:
+
+            self.raw = rawpy.imread(img)
+            self.img = self.raw.raw_image.copy()
+
+            if self.processes['linearization'] == True:
+                self.img = linearization(self.img)
+            if self.processes['demosaic'] == True:
+                self.img = demosaic(self.img)
+            if self.processes['denoising'] == True:
+                self.img = denoising(self.img, self.denoising_val)
+            if self.processes['colorSpace'] == True:
+                self.img = colorSpace(self.img, self.colour)
+            if self.processes['exposureComp'] == True:
+                self.img = exposureComp(self.img, self.exposure_val)
+            if self.processes['toneCurve'] == True:
+                self.img = toneCurve(self.img, self.tone_curve)
+            if self.processes['gammaCorrection'] == True:
+                self.img = gammaCorrection(self.img, self.gamma_correction)
+            if self.processes['greyWorldWB'] == True:
+                self.img = greyWorldWB(self.img, self.colour)
+            
+            return self.img ,imageResize(self.img, self.resize_val)
+
+        elif type(img) == np.ndarray:
+            
+            self.img = img
+
+            if self.processes['linearization'] == True:
+                self.img = linearization(self.img)
+            if self.processes['demosaic'] == True:
+                self.img = demosaic(self.img)
+            if self.processes['denoising'] == True:
+                self.img = denoising(self.img, self.denoising_val)
+            if self.processes['colorSpace'] == True:
+                self.img = colorSpace(self.img, self.colour)
+            if self.processes['exposureComp'] == True:
+                self.img = exposureComp(self.img, self.exposure_val)
+            if self.processes['toneCurve'] == True:
+                self.img = toneCurve(self.img, self.tone_curve)
+            if self.processes['gammaCorrection'] == True:
+                self.img = gammaCorrection(self.img, self.gamma_correction)
+            if self.processes['greyWorldWB'] == True:
+                self.img = greyWorldWB(self.img, self.colour)
+            
+            return self.img ,imageResize(self.img, self.resize_val)
 
     #method for returning the processed image.
     def getImage(self):
--- detect_laser.py
+++ detect_laser.py
@@ -4,6 +4,9 @@ import os
 import sys
 import numpy as np
 import rawpy
+import json
+
+from camera_imaging_pipeline.src.image_processing import imageProcessing
 
 from detect_laser_line import return_line
 
@@ -66,7 +69,7 @@ def detect_laser_raw(masked_raw: np.ndarray):
     return contours
 
 def display_masked_image(laser_path: Path, 
-                         calibration_path:Path, filepath: Path, raw: bool):
+                         calibration_path:Path, filepath: Path, raw: bool, param_path=None):
     
     if not raw:
         masked_image = get_masked_image_rgb(laser_path, calibration_path, filepath)
@@ -74,23 +77,54 @@ def display_masked_image(laser_path: Path,
         cv2.imshow("Masked_Window",masked_image)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
+
     else:
         masked_image = get_masked_image_raw(laser_path, calibration_path, filepath)
 
         contours = detect_laser_raw(masked_image)
-        masked_rgb = cv2.demosaicing(masked_image, cv2.COLOR_BAYER_GB2BGR)
+
+        # masked_rgb = cv2.demosaicing(masked_image, cv2.COLOR_BAYER_GB2BGR)
+        params = json.load(open(params_path))
+        processor = imageProcessing(params)
+        masked_rgb, _ = processor.applyToImage(masked_image)
+
         for cnt in contours:
-            cv2.drawContours(masked_rgb,[cnt],0,(0,0,255),thickness=-1)
-        resized = cv2.resize(masked_rgb, (1800, 1350))
-        # cv2.namedWindow("Masked_Window", cv2.WINDOW_NORMAL)
-        # cv2.imshow("Masked_Window",masked_rgb)
-        resized = cv2.resize(masked_rgb, (1800, 1350))
-        cv2.imshow("resized", resized)
+            cv2.drawContours(masked_rgb,[cnt],0,(0,0,65535),thickness=10)
+        cv2.namedWindow("Masked_Window", cv2.WINDOW_NORMAL)
+        cv2.imshow("Masked_Window",masked_rgb)
+        # resized = cv2.resize(masked_rgb, (1800, 1350))
+        cv2.imshow("resized", masked_rgb[1000:2000, 1000:3000])
         
         cv2.waitKey(0)
         cv2.destroyAllWindows()
         
+def display_detection(laser_path: Path, 
+                         calibration_path:Path, filepath: Path, params_path: Path):
 
+
+        masked_image = get_masked_image_raw(laser_path, calibration_path, filepath)
+
+        contours = detect_laser_raw(masked_image)   
+
+        params = json.load(open(params_path))
+        processor = imageProcessing(params)
+        processed_image, _ = processor.applyToImage(filepath.as_posix())
+        
+        # processed_image = processed_image.astype(np.uint8)
+        for cnt in contours:
+            cv2.drawContours(processed_image,[cnt],0,(0,0,65535),thickness=-1)
+        
+        resized = cv2.resize(processed_image, (1800, 1350))
+        cv2.imshow("resized", resized)
+        # cv2.namedWindow("Masked_Window", cv2.WINDOW_NORMAL)
+        # cv2.imshow("Masked_Window",masked_rgb)
+        
+        # cv2.imshow("cropped", processed_image[1000:2000, 1000:3000])
+        
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    
 laser_path = Path("/home/viva/Fishsense/fishsense-lite-python-pipeline/laser-calibration-output-4-12-bot.dat")
 calibration_path = Path("/home/viva/Fishsense/fishsense-lite-python-pipeline/calibration-output.dat")
 data_path = Path("/home/viva/Fishsense/fishsense-lite-python-pipeline/data/laser_mask_data")
@@ -101,4 +135,7 @@ for file in os.listdir(data_path.as_posix()):
     if filepath.suffix != ".ORF":
         continue
     
-    display_masked_image(laser_path, calibration_path, filepath, raw=True)
+    params_path = Path('/home/viva/Fishsense/fishsense-lite-python-pipeline/camera_imaging_pipeline/params1.json')
+
+    display_detection(laser_path, calibration_path, filepath, params_path)
+    # display_masked_image(laser_path, calibration_path, filepath, True, params_path)
