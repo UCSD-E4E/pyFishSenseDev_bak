5bcd5d320aca7ca0bf6a476e03c41e1bfbd66670
Christopher L. Crutchfield <ccrutchf@ucsd.edu>
2023-09-21 21:15:05 -0700

    export laser detector

--- fishsense_lite_python_pipeline/__init__.py
+++ fishsense_lite_python_pipeline/__init__.py
@@ -1,4 +1,5 @@
 from .fish_segmentation_inference import FishSegmentationInference
 from .fish_symmetry import FishSymmetry
 from .image_rectifier import ImageRectifier
+from .laser_detector import LaserDetector
 from .raw_processor import RawProcessor
