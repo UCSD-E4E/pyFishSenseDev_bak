fix: don't normalize vectors when averaging
We want to avoid normalizing since this effectively weights the vectors
by how far apart they are, which is ideal since longer differences mean
they tend toward the direction we are interested in.

laser_calibration.py
@@ -62,7 +62,8 @@ def atanasov_method(
         for j in range(ps.shape[0]):
             if i != j:
                 v = ps[i] - ps[j]
-                avg_alpha += v/np.linalg.norm(v)
+                if v[2] < 0: v = -v
+                avg_alpha += v
 
     avg_alpha /= np.linalg.norm(avg_alpha)
     if avg_alpha[2] < 0: