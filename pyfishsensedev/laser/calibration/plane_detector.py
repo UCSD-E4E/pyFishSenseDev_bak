39759e982217ee7f8cd4de7e9a85b7f468e24ed4
Christopher L. Crutchfield <ccrutchf@ucsd.edu>
2024-05-06 19:02:27 -0700

    feat: begin work on slate laser calibration

---
+++ pyfishsensedev/laser/calibration/plane_detector.py
@@ -0,0 +1,6 @@
+from abc import ABC
+
+
+class PlaneDetector(ABC):
+    def __init__(self) -> None:
+        super().__init__()
---
+++ pyfishsensedev/laser/calibration/slate_plane_detector.py
@@ -0,0 +1,6 @@
+from pyfishsensedev.laser.calibration.plane_detector import PlaneDetector
+
+
+class SlatePlaneDetector(PlaneDetector):
+    def __init__(self) -> None:
+        super().__init__()
