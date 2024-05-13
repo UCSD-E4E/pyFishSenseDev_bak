cde749a4f93e15eec74f09338575287e5421070a
Christopher L. Crutchfield <ccrutchf@ucsd.edu>
2024-05-13 10:02:31 -0700

    fix: remove additional debugging

--- tests/tests_setup.py
+++ tests/tests_setup.py
@@ -24,8 +24,6 @@ def get_creds() -> Dict[str, str]:
 def download_data(creds: Dict[str, str]):
     script_path = Path(__file__)
 
-    print(script_path.parent / "data")
-
     nas_unzip(
         network_path="smb://e4e-nas.ucsd.edu:6021/temp/github_actions/pyFishSenseDev/pyFishSenseDevTest.zip",
         output_path=script_path.parent / "data",
